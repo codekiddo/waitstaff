4f54bb19a507ecc7a544f22de9ec468c5b705c26
Lukas Weinwurm <lweinwurm@hotmail.com>
2014-05-28 10:59:40 +0200

    add customer charges

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-ap
+angular.module('myApp', [])
+  .controller('MainCtrl', function($scope) {
+    $scope.computeSubtotal = function(price, tax) {
+      if (price && tax) { 
+        return price + (price / 100 * tax);
+      } else {
+        return "";
+      }
+    };
+    $scope.computeTip = function(price, tip) {
+      if (price && tip) {
+        return (price / 100 * tip);
+      } else {
+        return "";
+      }
+    };
+
+    $scope.computeTotal = function(price, tax, tip) {
+      if (price && tax && tip) {
+        return $scope.computeSubtotal(price, tax) + $scope.computeTip(price, tip);
+      } else {
+        return "";
+      }  
+    }
+  });
